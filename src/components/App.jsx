9e0cd24513b389bdecf0a31a070310637c9da6b2
vitaliy-kro <krochakv6@gmail.com>
2022-10-25 12:30:55 +0300

    refactoring to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Section } from './Section';
 import { Box } from './Box';
 import { Phonebook } from './Phonebook';
@@ -6,67 +6,62 @@ import { Contacts } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
 import { getItemsFromLocalStorage, setItemsToLocalStorage } from './helpers';
 import { Loader } from './Loader';
-export class App extends Component {
-  state = {
-    contacts: [],
-    isLoading: false,
-    filter: '',
-  };
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    if (getItemsFromLocalStorage())
-      this.setState({ contacts: getItemsFromLocalStorage() });
-    this.setState({ isLoading: false });
-  }
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
 
-    if (prevState.contacts.length !== contacts.length) {
-      setItemsToLocalStorage(contacts);
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+    const itemsInLocalStorageCheck = getItemsFromLocalStorage().length > 0;
+    if (itemsInLocalStorageCheck) {
+      setContacts(getItemsFromLocalStorage());
     }
-  }
-  handleAddContact = obj => {
-    const previouslyAddedCheck = this.state.contacts.find(e => {
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    setItemsToLocalStorage(contacts);
+  }, [contacts]);
+
+  const handleAddContact = obj => {
+    const previouslyAddedCheck = contacts.find(e => {
       return e.name.toLowerCase() === obj.name.toLowerCase();
     });
-    if (previouslyAddedCheck)
+    if (previouslyAddedCheck) {
       return alert(`${previouslyAddedCheck.name} is already in contacts`);
-    this.setState(prev => {
-      return { contacts: [...prev.contacts, obj] };
-    });
+    }
+
+    setContacts(prev => [...prev, obj]);
   };
-  handleDeleteContact = id => {
-    this.setState(prev => {
-      return { contacts: prev.contacts.filter(e => e.id !== id) };
-    });
+  const handleDeleteContact = id => {
+    setContacts(prev => prev.filter(e => e.id !== id));
   };
-  changeFilter = ({ target }) => {
-    this.setState({ filter: target.value });
-  };
-  visibleNumbers = () => {
-    const { contacts, filter } = this.state;
+
+  const changeFilter = ({ target }) => setFilter(target.value);
+
+  const visibleNumbers = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(({ name }) => {
       return name.toLowerCase().includes(normalizedFilter);
     });
   };
 
-  render() {
-    const getVisibleContacts = this.visibleNumbers();
-    return (
-      <Box p="4">
-        <Section title="Phonebook">
-          <Phonebook addContact={this.handleAddContact}></Phonebook>
-        </Section>
-        <Section title="Contacts">
-          <Filter filter={this.changeFilter}></Filter>
-          <Contacts
-            markup={getVisibleContacts}
-            deleteContact={this.handleDeleteContact}
-          ></Contacts>
-          {this.state.isLoading && <Loader></Loader>}
-        </Section>
-      </Box>
-    );
-  }
-}
+  const getVisibleContacts = visibleNumbers();
+  return (
+    <Box p="4">
+      <Section title="Phonebook">
+        <Phonebook addContact={handleAddContact}></Phonebook>
+      </Section>
+      <Section title="Contacts">
+        <Filter filter={changeFilter}></Filter>
+        <Contacts
+          markup={getVisibleContacts}
+          deleteContact={handleDeleteContact}
+        ></Contacts>
+        {isLoading && <Loader></Loader>}
+      </Section>
+    </Box>
+  );
+};
